af83014bf57be081b2b41d3fce6b674ec3ee990c
Mayeul LE MONIES DE SAGAZAN <mle-moni@student.42.fr>
2020-02-22 14:58:08 +0100

    deleted lines to match bash result

--- srcs/builtins/pwd/ft_pwd.c
+++ srcs/builtins/pwd/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 11:28:58 by mle-moni          #+#    #+#             */
-/*   Updated: 2020/02/22 13:03:02 by mle-moni         ###   ########.fr       */
+/*   Updated: 2020/02/22 14:56:37 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,9 @@ int	ft_pwd(int ac, char **av, t_list **env_list)
 {
 	char	*cwd;
 
+	(void)ac;
 	(void)av;
 	(void)env_list;
-	if (ac > 1)
-	{
-		ft_putstr_fd("pwd: too many arguments\n", 2);
-		return (1);
-	}
 	if (!(cwd = getcwd(NULL, 0)))
 	{
 		ft_putstr_fd("pwd: cwd exceeds PATH_MAX\n", 2);
