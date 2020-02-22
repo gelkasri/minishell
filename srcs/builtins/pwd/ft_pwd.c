d4f4d1e1a1f0f832764516add8a38368a35cea47
Mayeul LE MONIES DE SAGAZAN <mle-moni@student.42.fr>
2020-02-22 13:05:49 +0100

    fixed ft_pwd leaks

--- srcs/builtins/pwd/ft_pwd.c
+++ srcs/builtins/pwd/ft_pwd.c
@@ -6,28 +6,30 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 11:28:58 by mle-moni          #+#    #+#             */
-/*   Updated: 2020/02/22 12:01:00 by mle-moni         ###   ########.fr       */
+/*   Updated: 2020/02/22 13:03:02 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <errno.h>
-#define PATH_MAX 256
 
 int	ft_pwd(int ac, char **av, t_list **env_list)
 {
-	char	cwd_buffer[PATH_MAX];
+	char	*cwd;
 
+	(void)av;
+	(void)env_list;
 	if (ac > 1)
 	{
 		ft_putstr_fd("pwd: too many arguments\n", 2);
 		return (1);
 	}
-	if (!getcwd(cwd_buffer, PATH_MAX))
+	if (!(cwd = getcwd(NULL, 0)))
 	{
 		ft_putstr_fd("pwd: cwd exceeds PATH_MAX\n", 2);
 		return (1);
 	}
-	ft_putendl_fd(cwd_buffer, 1);
+	ft_putendl_fd(cwd, 1);
+	free(cwd);
 	return (0);
 }
