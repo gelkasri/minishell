341cb2809c145710f640cee73b8128b9f066df1f
Mayeul LE MONIES DE SAGAZAN <mle-moni@student.42.fr>
2020-02-24 12:45:45 +0100

    export bettered (like bash except =)

--- srcs/builtins/export/ft_export.c
+++ srcs/builtins/export/ft_export.c
@@ -6,11 +6,39 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 14:39:53 by mle-moni          #+#    #+#             */
-/*   Updated: 2020/02/22 16:46:18 by mle-moni         ###   ########.fr       */
+/*   Updated: 2020/02/24 12:30:49 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	ft_str_is_alpha_num(const char *str)
+{
+	int	i;
+	int	char_is_ok;
+
+	i = 0;
+	while (str[i])
+	{
+		char_is_ok = ft_isdigit(str[i]) || ft_isalpha(str[i]);
+		if (!char_is_ok)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	check_errors(const char *str, const char *error_param)
+{
+	if (!str[0] || ft_isdigit(str[0]) || !ft_str_is_alpha_num(str))
+	{
+		ft_putstr_fd("export: `", 2);
+		ft_putstr_fd(error_param, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
+		return (1);
+	}
+	return (0);
+}
 
 /*
 ** parse_and_export takes a string formatted as follow:
@@ -18,12 +46,14 @@
 ** (=VALUE is facultative)
 */
 
-int		parse_and_export(char *str, t_list **env_list)
+int			parse_and_export(char *str, t_list **env_list)
 {
 	char	**split;
 
 	split = ft_split(str, '=');
 	if (!split)
+		return (1);
+	if (check_errors(split[0], str))
 		return (1);
 	if (split[0] && split[1])
 		set_env_var(split[0], split[1], env_list);
@@ -39,11 +69,13 @@ int		parse_and_export(char *str, t_list **env_list)
 ** if ac=1, it displays the environment variables
 */
 
-int		ft_export(int ac, char **av, t_list **env_list)
+int			ft_export(int ac, char **av, t_list **env_list)
 {
-	int i;
+	int	i;
+	int	ret;
 
 	i = 1;
+	ret = 0;
 	if (ac == 1)
 	{
 		ft_env(ac, av, env_list);
@@ -51,8 +83,8 @@ int		ft_export(int ac, char **av, t_list **env_list)
 	}
 	while (ac > i)
 	{
-		parse_and_export(av[i], env_list);
+		ret |= parse_and_export(av[i], env_list);
 		i++;
 	}
-	return (0);
+	return (ret);
 }
