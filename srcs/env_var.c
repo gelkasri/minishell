2256d91430023b3136f40ef5fd0d8dc130e34210
Mayeul LE MONIES DE SAGAZAN <mle-moni@student.42.fr>
2020-02-26 09:43:52 +0100

    seems ok

--- srcs/env_var.c
+++ srcs/env_var.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 15:42:22 by mle-moni          #+#    #+#             */
-/*   Updated: 2020/02/25 19:10:41 by mle-moni         ###   ########.fr       */
+/*   Updated: 2020/02/26 09:04:21 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,46 @@
 
 #include <stdio.h>
 
-//use trim_this_part !!??
+static char	*replace_at_pos(char *str, int pos, int oldlen, char *var)
+{
+	char	*new;
+	int		new_pos;
+
+	new = ft_memalloc(ft_strlen(str) - oldlen + 1 + ft_strlen(var));
+	if (!new)
+	{
+		free(str);
+		return (NULL);
+	}
+	ft_strncpy(new, str, pos);
+	ft_strcpy(new + pos, var);
+	new_pos = pos + ft_strlen(var);
+	ft_strcpy(new + new_pos, str + pos + oldlen);
+	free(str);
+	return (new);
+}
 
 static char	*replace_env_var(char *str, int index, t_list **env_list)
 {
 	int		i;
 	char	*ret;
 	char	*substr;
-	char	*test;
+	char	*var;
 
-	printf("--------------------------\n");
 	i = index + 1;
 	while (ft_isalpha(str[i]) || ft_isdigit(str[i]))
 		i++;
+	if (i == index + 1)
+		return (str);
 	substr = ft_substr(str, index + 1, i - (index + 1));
-	printf("substr = %s\n", substr);
-	test = get_env_var(substr, env_list);
-	printf("env_var = %s\n", test);
-	free(test);
+	var = get_env_var(substr, env_list);
+	if (!var)
+		var = ft_strdup("");
+	ret = replace_at_pos(str, index, ft_strlen(substr) + 1, var);
+	free(var);
 	free(substr);
-	ret = ft_strdup(str);
-	free(str);
-	printf("--------------------------\n");
+	if (!ret)
+		return (NULL);
 	return (ret);
 }
 
@@ -55,7 +73,10 @@ static int	get_next_match(char *str)
 		else if (str[i] == '\'' && !in_dquote)
 			in_quote = (in_quote + 1) % 2;
 		else if (str[i] == '$' && !in_quote)
-			return (i);
+		{
+			if (ft_isalpha(str[i + 1]) || ft_isdigit(str[i + 1]))
+				return (i);
+		}
 		i++;
 	}
 	return (-1);
@@ -80,12 +101,11 @@ char		*apply_env_var(char *str, t_list **env_list)
 	final = ft_strdup(str);
 	if (!final)
 		return (NULL);
-	// mettre un while
-	if ((last_index = get_next_match(final)) != -1)
+	while ((last_index = get_next_match(final)) != -1)
 	{
 		final = replace_env_var(final, last_index, env_list);
 		if (!final)
-			return (NULL);
+			return (ft_strdup(""));
 	}
 	return (final);
 }
