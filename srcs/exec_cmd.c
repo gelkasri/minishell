91d674790b0b1907aed3a781fc201b7f3f3d8773
Guillaume EL KASRI <gel-kasr@student.42.com>
2020-02-28 11:34:15 +0100

    Pipes working - With a few norm errors

--- srcs/exec_cmd.c
+++ srcs/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 14:57:20 by gel-kasr          #+#    #+#             */
-/*   Updated: 2020/02/26 12:06:10 by gel-kasr         ###   ########.fr       */
+/*   Updated: 2020/02/28 11:07:11 by gel-kasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,25 @@ static int		free_and_return(char ***ptr, int ret_val)
 
 static int		handle_error(int error_type, const char *cmd)
 {
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(cmd, 2);
+	if (cmd)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(cmd, 2);
+	}
 	if (error_type == 1)
 	{
-		ft_putendl_fd(": command not found", 2);
+		if (cmd)
+			ft_putendl_fd(": command not found", 2);
 		return (127);
 	}
 	else
 	{
-		ft_putstr_fd(cmd, 2);
-		ft_putstr_fd(": ", 2);
-		ft_putendl_fd(strerror(errno), 2);
+		if (cmd)
+		{
+			ft_putstr_fd(cmd, 2);
+			ft_putstr_fd(": ", 2);
+			ft_putendl_fd(strerror(errno), 2);
+		}
 		return (errno);
 	}
 }
@@ -76,6 +83,91 @@ static int		exec_cmd(char *cmd, t_list **env_list)
 	return (free_and_return(&split, ret));
 }
 
+static int		split_len(char **split)
+{
+	int	i;
+
+	if (!split)
+		return (0);
+	i = 0;
+	while (split[i])
+		i++;
+	return (i);
+}
+
+static void		trim_path(char **split)
+{
+	int		i;
+	char	*path;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		path = ft_strtrim(split[i], " ");
+		free(split[i]);
+		split[i] = ft_strdup(path);
+		free(path);
+		i++;
+	}
+}
+
+static int		pipe_loop(char **cmds, int nb_pipes, t_list **env_list)
+{
+	int		i;
+	int		outputfd;
+	pid_t	pid;
+	int		pipefd[2];
+	int		ret;
+
+	i = 0;
+	while (i < nb_pipes)
+	{
+		pipe(pipefd);
+		pid = fork();
+		if (pid == 0)
+		{
+			close(pipefd[0]);
+			if (i != 0)
+				dup2(outputfd, 0);
+			if (nb_pipes != i + 1)
+				dup2(pipefd[1], 1);
+			close(pipefd[1]);
+			close(outputfd);
+			exit(exec_cmd(cmds[i], env_list));
+		}
+		close(pipefd[1]);
+		outputfd = dup(pipefd[0]);
+		close(pipefd[0]);
+		waitpid(pid, &ret, 0);
+		ret = get_child_exit_status(ret);
+		i++;
+	}
+	return (ret);
+}
+
+static int		exec_pipe(char *cmd, t_list **env_list)
+{
+	int		ret;
+	char	**split;
+	int		nb_pipes;
+
+	if (ft_index(cmd, '|') < 0)
+	{
+		ret = exec_cmd(cmd, env_list);
+		return (ret);
+	}
+	split = ft_split(cmd, '|');
+	if (!(nb_pipes = split_len(split)))
+		return (MALLOC_ERROR);
+	trim_path(split);
+	ret = pipe_loop(split, nb_pipes, env_list);
+	free_str_arr(split);
+	free(split);
+	return (ret);
+}
+
 int				exec_line(char *line, t_list **env_list)
 {
 	char	**commands;
@@ -96,7 +188,7 @@ int				exec_line(char *line, t_list **env_list)
 		if (!tmp)
 			ret = 1;
 		else
-			ret = exec_cmd(tmp, env_list);
+			ret = exec_pipe(tmp, env_list);
 		free(tmp);
 	}
 	free_str_arr(commands);
