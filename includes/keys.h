687d7d321a0a7ca9cde73db91595d496204a81f4
Guillaume El Kasri <guillaume.elkasri@gmail.com>
2020-03-01 12:18:31 +0100

    Init arrow key press management

--- includes/keys.h
+++ includes/keys.h
@@ -2,6 +2,10 @@
 #ifndef _KEYS_H
 # define _KEYS_H
 
-# define ENTER_KEY 13
+# define ENTER_KEY	13
+# define UP_KEY		'A'
+# define DOWN_KEY	'B'
+# define RIGHT_KEY	'C'
+# define LEFT_KEY	'D'
 
 #endif
--- srcs/termios/reader.c
+++ srcs/termios/reader.c
@@ -1,17 +1,50 @@
 
 #include "minishell.h"
 
-static char		read_key(void)
+static void		arrow_key_press(char c, t_editor *editor)
+{
+	editor->pos = get_cur_pos();
+	(void)editor;
+	if (c == UP_KEY)
+		editor->pos.y--;
+	if (c == DOWN_KEY)
+		editor->pos.y++;
+	if (c == RIGHT_KEY)
+		editor->pos.x++;
+	if (c == LEFT_KEY)
+		editor->pos.x--;
+	set_cur_pos(editor->pos.x, editor->pos.y);
+}
+
+/*
+** Fonction that read keys pressed on keyboard
+** \x1b = escape code for special presses (arrow keys)
+** arrows keys are on 3 bytes : \x1b + '[' + 'A|B|C|D'
+*/
+
+static char		read_key(t_editor *editor)
 {
 	int		nread;
 	char	c;
+	char	seq[3];
 
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
 		if (nread < 0)
 			editor_error("read_key");
 	if (nread == 0)
 		return (0);
-	return (c);
+	if (c == '\x1b')
+	{
+		if (read(STDIN_FILENO, &seq[0], 1) != 1)
+			return ('\x1b');
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+			return ('\x1b');
+		if (seq[0] == '[')
+			arrow_key_press(seq[1], editor);
+		return ('\x1b');
+	}
+	else
+		return (c);
 }
 
 static int		add_to_editor_buffer(t_editor *editor, char c)
@@ -34,7 +67,7 @@ static int		process_key_press(t_editor *editor)
 {
 	char	c;
 
-	c = read_key();
+	c = read_key(editor);
 	if (c == 0)
 		return (0);
 	if (c == ctrl_key('d'))
