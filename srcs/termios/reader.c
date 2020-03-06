aef6c4781b7ebd2fcf356c19c3239ce167654d86
Guillaume EL KASRI <gel-kasr@student.42.com>
2020-03-06 15:06:07 +0100

    Disable Ctrl+D when buffer is not empty

--- srcs/termios/reader.c
+++ srcs/termios/reader.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 10:59:07 by gel-kasr          #+#    #+#             */
-/*   Updated: 2020/03/06 11:20:24 by mle-moni         ###   ########.fr       */
+/*   Updated: 2020/03/06 15:04:37 by gel-kasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ static int		process_shortcuts(char c, t_editor *editor, t_list **env_list)
 {
 	if (c == 0)
 		return (0);
-	else if (c == ctrl_key('d'))
+	else if (c == ctrl_key('d') && !editor->buf[0])
 		return (-1);
 	else if (c == ctrl_key('k'))
 		cut_copy_after_cursor(1, editor);
